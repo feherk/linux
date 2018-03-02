6e5f3d42793a1aa093a12cd0af8d32799bad56f1
Feher Karoly <fk@codealfa.hu>
2018-03-02 15:14:29 +0100

    webmin install batch

---
+++ webmin_install.sh
@@ -0,0 +1,3 @@
+sudo dpkg --install ./webmin_1.870_all.deb
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+sudo apt --fix-broken install
